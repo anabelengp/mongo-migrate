0181cf32d5d203bfa86b04afbc149c090bace2f7
Ana <anabelengp@gmail.com>
2017-11-03 16:56:09 +0100

    allow version per project in same database

--- get_current.js
+++ get_current.js
@@ -1,4 +1,6 @@
-var migration_info = db.migration_info.findOne();
+var query = (!!project) ? {project: project} : {};
+
+var migration_info = db.migration_info.findOne(query);
 if (migration_info) {
   print(migration_info.version);
 } else {
